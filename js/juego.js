2ba2010cc1cfc197156bc4804f64d32ce7332553
Joel Gaytan Mendez <71796345+JoelGaytan33@users.noreply.github.com>
2020-12-28 20:18:35 -0700

    json
    json

--- js/juego.js
+++ js/juego.js
@@ -1,11 +1,32 @@
 //me aseguro de que ha cargado toda la pagina
+var inideceRandom=0;
+var opc=[];
 $(document).ready(function(){
-    $("#login").animate({
+   /* $("#login").animate({
         'top':"10%"
-    },200);
+    },300);*/
+    $.getJSON('https://raw.githubusercontent.com/Bowserinator/Periodic-Table-JSON/master/PeriodicTableJSON.json',
+    function(data){
+        //console.log(data.elements[0].name);
+        var cuantos = data.elements.length;
+        var random=Math.floor(Math.random() * cuantos);
+        console.log(random);
+        $("#elemento").find('h2').text( data.elements[random].name);
+
+            for(var i=0;i<3;i++){
+                var n=Math.floor(Math.random() * cuantos);
+                opc.push(n);
+                $("#opciones").find('ul').append('<li><h2>'+ data.elements[n].symbol +'</h2></>');
+            }
+            $("#opciones").find('ul').append('<li><h2>'+ data.elements[n].symbol +'</h2></>');
+            opc.push(inideceRandom);
+            console.log(opc);
+     
+        });
     $("#btnIniciar").click(function(){
         if($("txtNombre").val() == ""){
             $("txtNombre").css('border', '1px solid red')
+            $(".alerta").fadeIn(400);
 
             }else{
                 $("#login").animate({
@@ -15,7 +36,7 @@ $(document).ready(function(){
                 $("#login").fadeOut(1000);
             });
             }
-        }
+        
     
-});
+
 });
